7b7e37529006cb6c273591974e32b89db3ef5797
Helaly96 <zfaesu@gmail.com>
2020-04-16 15:39:48 +0200

    2 Stadium Halves Segmentation,Applied Basic Filtering

--- Stadium_Segmentation.py
+++ Stadium_Segmentation.py
@@ -1,10 +1,31 @@
 import numpy as np
 import cv2
-import imutils
+
+
+def Crop_Image(event, x, y, flags, param):
+    global cropping
+    global end_drawing
+    global current_pos
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        cv2.circle(frame, (x, y), 10, (255, 0, 0), -1)
+        points.append((x, y))
+        cropping = True
+
+    elif event == cv2.EVENT_LBUTTONUP:
+        points.append((x, y))
+        cropping = False
+        cv2.rectangle(frame, points[0], points[1], (0, 255, 0), 2)
+        end_drawing = True
+
+    elif event == cv2.EVENT_MOUSEMOVE and cropping:
+        current_pos = (x, y)
+
+
 
 def Stadium_segment(image):
-    lower_stadium = np.array([100,80,120], dtype=np.uint8)
-    upper_stadium = np.array([110,110,190], dtype=np.uint8)
+    lower_stadium = np.array([100,80,90], dtype=np.uint8)
+    upper_stadium = np.array([110,110,185], dtype=np.uint8)
     # Threshold the HSV image to get only white colors
     mask = cv2.inRange(image, lower_stadium, upper_stadium)
     # Bitwise-AND mask and original image
@@ -103,7 +124,7 @@ def Bounding_Box_Of_Stadium(c,image_to_be_displayed):
                 lineType
                 )
 
-        elif(abs(y-prev_point[1]) >0.7*height_of_rectangle):
+        elif(abs(y-prev_point[1]) >0.4*height_of_rectangle):
             count+=1
             cv2.circle(image_to_be_displayed, (x,y), 8, (0, 0, 255), -1)
             prev_point = list((x,y))
@@ -129,19 +150,54 @@ def color_pick(event,x,y,flags,param):
         print("Value of clicked position is")
         print(frame[y,x])
 
+
+#to hold image of rect
+points=[]
+
+cropping = False
+
+#current mouse position
+current_pos=(0,0)
+end_drawing=False
+
+#define a window
+cv2.namedWindow('Original_First_Frame')
+#the window the mouse events binded to that windows
+cv2.setMouseCallback('Original_First_Frame',Crop_Image)
+
 #read from video
 cap = cv2.VideoCapture('Testing-pingpong.mp4')
 
+#read it
+clone = cv2.imread("x.jpg")
+#keep showing the image, so we can draw on it hehe.
+while(1):
+    frame = clone.copy()
+    if(cropping and current_pos!=(0,0)):
+        cv2.rectangle(frame, points[0], current_pos, (255, 0, 0), 2)
+    k = cv2.waitKey(20) & 0xFF
+    if end_drawing:
+        break
+    cv2.imshow('Original_First_Frame',frame)
+
+
+#destroy the first frame
+cv2.destroyAllWindows()
+
+
+
+
 saved_frame=[]
-
 no_of_frames_to_be_considered=700
-
-#Capture the first selected_no frames 
+#Capture the first selected_no frames
 while(1):
     ret, frame = cap.read()
     frame = cvt_hsv(frame)
+    frame= frame[points[0][1]:points[1][1], points[0][0]:points[1][0]]
     mask = Stadium_segment(frame)
-    if(len(saved_frame) < no_of_frames_to_be_considered):
+    kernel = np.ones((3, 3), np.uint8)
+    closing = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+    if len(saved_frame) < no_of_frames_to_be_considered:
         saved_frame.append(mask)
     else:
         break
@@ -159,11 +215,21 @@ for i in range(len(saved_frame)):
     contours, _ = cv2.findContours(result,  
     cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     contours = sorted(contours, key = cv2.contourArea, reverse = True)[:4]
-    hull = cv2.convexHull(contours[0])
-    cv2.drawContours(image_to_be_displayed, [hull],-1, (255, 255,255), 3)     
-    pts,image_to_be_displayed = Bounding_Box_Of_Stadium(hull,image_to_be_displayed)
+
+    Right_Half = cv2.convexHull(contours[0])
+    Left_Half = cv2.convexHull(contours[1])
+
+
+    cv2.drawContours(image_to_be_displayed, [Right_Half],-1, (255, 255,255), 3)
+    cv2.drawContours(image_to_be_displayed, [Left_Half], -1, (255, 0, 0), 3)
+
+
+    pts,image_to_be_displayed = Bounding_Box_Of_Stadium(Right_Half,image_to_be_displayed)
+    pts, image_to_be_displayed = Bounding_Box_Of_Stadium(Left_Half, image_to_be_displayed)
+
     no_of_corner.append(pts)
     displayed_frame.append(image_to_be_displayed)
+
     Area=0
     for j in range(len(contours)):
         Area += cv2.contourArea(contours[j])
---
+++ Testing_Ball_HSV/color_picking.py
@@ -0,0 +1,27 @@
+import numpy as np
+import cv2
+
+def color_pick(event,x,y,flags,param):
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        print("Value of clicked position is")
+        print(hsv[y,x])
+
+def cvt_hsv(image):
+    image = cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
+    return image
+
+#define a window
+cv2.namedWindow('pick')
+#the window the mouse events binded to that windows
+cv2.setMouseCallback('pick',color_pick)
+image = cv2.imread("ball.png")
+
+while(1):
+    frame = image
+    hsv = cvt_hsv(frame)
+    k = cv2.waitKey(20) & 0xFF
+    if(k == ord('e') ):
+        break
+    cv2.imshow("pick",image)
+    cv2.imshow("hsv",hsv)
+
