0ae1b1444d1b74d43f3d1393a960548ab95e2ee1
Omar Ahmad <omarahmad293@gmail.com>
2020-04-25 23:45:40 +0200

    Updated BallTrack.py

--- BallTrack.py
+++ BallTrack.py
@@ -1,152 +1,159 @@
 import numpy as np
 import cv2
 
+''' -----------------/ The Bounding Box /--------------------'''
 
 
-#-----------------/ The Bounding Box /--------------------
-
-
-
-#to hold image of rect
-points=[]
+# to hold image of rect
+points = []
 
 cropping = False
 
-#current mouse position
-current_pos=(0,0)
-end_drawing=False
+# current mouse position
+current_pos = (0, 0)
+end_drawing = False
 
-#the mouse call back event , i binded it to the very first frame
-#basically i want the user to click 4 points, that will be the 
-#ROI of interest
-#THIS IS JUST FOR TESTING, THE ROI will be the table of pingpong
 
-def Crop_Image(event,x,y,flags,param):
+# the mouse call back event , i binded it to the very first frame
+# basically i want the user to click 4 points, that will be the
+# ROI of interest
+# THIS IS JUST FOR TESTING, THE ROI will be the table of pingpong
+
+def Crop_Image(event, x, y, flags, param):
     global cropping
     global end_drawing
     global current_pos
 
     if event == cv2.EVENT_LBUTTONDOWN:
-        cv2.circle(frame,(x,y),10,(255,0,0),-1)
-        points.append( (x,y) )
+        cv2.circle(frame, (x, y), 10, (255, 0, 0), -1)
+        points.append((x, y))
         cropping = True
 
     elif event == cv2.EVENT_LBUTTONUP:
-        points.append((x,y))
+        points.append((x, y))
         cropping = False
         cv2.rectangle(frame, points[0], points[1], (0, 255, 0), 2)
-        end_drawing=True
+        end_drawing = True
 
-    elif event == cv2.EVENT_MOUSEMOVE and cropping :
-        current_pos=(x,y)
-		
+    elif event == cv2.EVENT_MOUSEMOVE and cropping:
+        current_pos = (x, y)
 
-		# draw a rectangle around the region of interest
+    # draw a rectangle around the region of interest
 
-#Color Filtering Function
+
+# Color Filtering Function
 
 def colorSegment(frame):
-    lower = np.array([80,0,0])
-    upper = np.array([120,100,255])
+    lower = np.array([80, 0, 0])
+    upper = np.array([120, 100, 255])
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(hsv, lower, upper)
-    res = cv2.bitwise_and(frame,frame, mask = mask)
+    res = cv2.bitwise_and(frame, frame, mask=mask)
     return res
 
 
-
-#define a window
+# define a window
 cv2.namedWindow('Original_First_Frame')
-#the window the mouse events binded to that windows
-cv2.setMouseCallback('Original_First_Frame',Crop_Image)
+# the window the mouse events binded to that windows
+cv2.setMouseCallback('Original_First_Frame', Crop_Image)
 
-#read from video
+# read from video
 cap = cv2.VideoCapture('Edmonton.mp4')
 
-#read first frame of video
-ret,frame = cap.read()
+# read first frame of video
+ret, frame = cap.read()
 
-#write the first frame
+# write the first frame
 cv2.imwrite("Testing_Ball_HSV/x.jpg", frame)
 
-#read it
+# read it
 clone = cv2.imread("Testing_Ball_HSV/x.jpg")
 
-#keep showing the image, so we can draw on it hehe.
-while(1):
+all_contours = []
+
+# keep showing the image, so we can draw on it hehe.
+while True:
     frame = clone.copy()
-    if(cropping and current_pos!=(0,0)):
+    if cropping and current_pos != (0, 0):
         cv2.rectangle(frame, points[0], current_pos, (255, 0, 0), 2)
     k = cv2.waitKey(20) & 0xFF
     if end_drawing:
         break
-    cv2.imshow('Original_First_Frame',frame)
+    cv2.imshow('Original_First_Frame', frame)
 
-
-#destroy the first frame
+# destroy the first frame
 cv2.destroyAllWindows()
 cap.release()
 
+''' --------------------/ Video Processing /------------------ '''
 
+# Parameters for the difference
+sensitivityValue1 = 40
+sensitivityValue2 = 75
+blurSize = (15, 15)
 
-#--------------------/ Video Processing /------------------
-
-
-
-#Parameters for the difference
-sensitivityValue1 = 80
-sensitivityValue2 = 80
-blurSize = (9,9)
-
-#read from video
+# read from video
 cap = cv2.VideoCapture('Edmonton.mp4')
 _, frame = cap.read()
 frame = frame[points[0][1]:points[1][1], points[0][0]:points[1][0]]
-#frame = colorSegment(frame)
+# frame = colorSegment(frame)
 grayImage = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 previous = grayImage.copy()
 
-while(1):
+
+while True:
     _, frame = cap.read()
+    if frame is None:
+        break
+
     frame = frame[points[0][1]:points[1][1], points[0][0]:points[1][0]]
-    #frame = colorSegment(frame)
+    # frame = colorSegment(frame)
     grayImage = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     differenceImage = cv2.absdiff(grayImage, previous)
-    _, thresholdImage = cv2.threshold(differenceImage, sensitivityValue1, 255, cv2.THRESH_BINARY)
-	
-    #Blurring the Image to get rid of noise
-    finalThresholdImage = cv2.GaussianBlur(thresholdImage, blurSize,cv2.BORDER_DEFAULT)
-    _, finalThresholdImage = cv2.threshold(finalThresholdImage, sensitivityValue2, 255, cv2.THRESH_BINARY)
+    blur = cv2.GaussianBlur(differenceImage, (5, 5), cv2.BORDER_DEFAULT)
+    _, thresholdImage = cv2.threshold(blur, sensitivityValue1, 255, cv2.THRESH_BINARY)
 
-    #Opening
-    structuringElementSize = (10,10)
+    # Blurring the Image to get rid of noise
+    #finalThresholdImage = cv2.GaussianBlur(thresholdImage, blurSize, cv2.BORDER_DEFAULT)
+    #_, finalThresholdImage = cv2.threshold(finalThresholdImage, sensitivityValue2, 255, cv2.THRESH_BINARY)
+
+    # Opening
+    structuringElementSize = (10, 10)
     structuringElement = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, structuringElementSize)
-    finalThresholdImage = cv2.morphologyEx(finalThresholdImage, cv2.MORPH_OPEN, structuringElement)
-    
+    finalThresholdImage = cv2.morphologyEx(thresholdImage, cv2.MORPH_OPEN, structuringElement)
 
-    #Contour Detection
-    #Contour Parameters
-    perimeterMin = 40
+    # Contour Detection
+    # Contour Parameters
+    perimeterMin = 50
     perimeterMax = 100
     epsilon = 0.03
     numberOfAcceptedContours = 4
 
-    #Blank frame to draw the contour on
+    # Blank frame to draw the contour on
     blankFrame = np.zeros(frame.shape)
 
     contours, hierarchy = cv2.findContours(finalThresholdImage, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     contours = sorted(contours, key=cv2.contourArea, reverse=True)[:numberOfAcceptedContours]
-    real_cnts=[]
+    real_cnts = []
 
     for cnt in contours:
         approx = cv2.approxPolyDP(cnt, epsilon * cv2.arcLength(cnt, True), True)
         perimeter = cv2.arcLength(cnt, True)
-        if(len(approx) > 3 and perimeter > perimeterMin and perimeter < perimeterMax):
-            k = cv2.isContourConvex(approx)
-            if(k):
-                real_cnts.append((cnt))
-    
-    cv2.drawContours(frame, real_cnts, -1, (0, 0, 255), 3)
+        if len(approx) > 5 and perimeterMin < perimeter < perimeterMax:
+            #if cv2.isContourConvex(approx):
+            if cv2.contourArea(cnt) < 300:
+                real_cnts.append(cnt)
+
+    all_contours.append(real_cnts)
+    contours_only = np.zeros(frame.shape)
+    contoured = cv2.cvtColor(finalThresholdImage, cv2.COLOR_GRAY2RGB)
+
+    for c in all_contours:
+        cv2.drawContours(contoured, c, -1, (0, 0, 255), thickness=cv2.FILLED)
+        cv2.drawContours(contours_only, c, -1, (0, 0, 255), thickness=cv2.FILLED)
+        cv2.drawContours(frame, c, -1, (0, 0, 255), thickness=cv2.FILLED)
+
+    #ball_only = cv2.bitwise_and(frame, contours_only)
 
     '''
     #Apply Hough Circle
@@ -170,30 +177,31 @@ while(1):
                 cv2.rectangle(frame, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
     '''
 
-    #Window Showing
+    # Window Showing
 
-    #cv2.imshow('Difference Image',differenceImage)
-    #cv2.imshow('Threshold Image',thresholdImage)
+    #cv2.imshow('Difference Image', differenceImage)
+    #cv2.imshow('Threshold Image', thresholdImage)
     #cv2.imshow('Final Threshold Image', finalThresholdImage)
-    cv2.imshow('Contour Detected', frame)
+    #cv2.imshow('Contour only', contours_only)
+    #cv2.imshow('Contours', contoured)
+    cv2.imshow('Contour Detected on original', frame)
 
-
-    previous = grayImage.copy() 
+    previous = grayImage.copy()
 
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
-    elif k==ord('h'):
-        h_lower=int(input("Enter Lower Hue's Value \n"))
-        h_higher=int(input("Enter Upper Hue's Value \n"))
-    elif k==ord('v'):
-        v_lower=int(input("Enter Lower Value's Value \n"))
-        v_higher=int(input("Enter Lower Value's Value \n"))
-    elif k==ord('s'):
-        s_lower=int(input("Enter Lower Saturation's Value \n"))
-        s_higher=int(input("Enter Lower Saturation's Value \n"))
-    elif k==ord('p'):
-        x=input()
+    elif k == ord('h'):
+        h_lower = int(input("Enter Lower Hue's Value \n"))
+        h_higher = int(input("Enter Upper Hue's Value \n"))
+    elif k == ord('v'):
+        v_lower = int(input("Enter Lower Value's Value \n"))
+        v_higher = int(input("Enter Lower Value's Value \n"))
+    elif k == ord('s'):
+        s_lower = int(input("Enter Lower Saturation's Value \n"))
+        s_higher = int(input("Enter Lower Saturation's Value \n"))
+    elif k == ord('p'):
+        x = input()
 
 cap.release()
 cv2.destroyAllWindows()
